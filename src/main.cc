9de5a5e800f651dbbf51e1783a62bd3c85434d8d
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-02 21:39:25 -0700

    just saving

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,21 @@
-//
-// Created by Rob on 11/2/2018.
-//
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex04-mergesort
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
 
+#include <iostream>
+#include "fibonacci.h"
+using edu::vcccd::vc::csv15::mergesort;
 
+int main(int argc, char *argv[]) {
+    mergesort();
+    exit(0);
+}
 
