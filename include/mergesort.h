a9b010e2b013cdef2eec2996b63393edcbaeb4d9
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-02 21:36:05 -0700

    just saving

---
+++ include/mergesort.h
@@ -0,0 +1,32 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex04-mergesort
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
+
+#ifndef EX04_MERGESORT_MERGESORT_H
+#define EX04_MERGESORT_MERGESORT_H
+
+#include <cstdint>
+
+namespace edu {
+    namespace vcccd{
+        namespace vc{
+            namespace csv15{
+                template<typename>
+                    void mergesort(T array[], size_t size){
+                        return;
+                }
+            }
+        }
+    }
+}
+
+#endif //EX04_MERGESORT_MERGESORT_H
