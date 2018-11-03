07ddaacef5858ab40b4c525055d18f9d0b1e6a17
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-02 23:15:35 -0700

    just messing around, still using 3 inputs mergesort

--- include/mergesort.h
+++ include/mergesort.h
@@ -26,11 +26,13 @@ namespace edu {
 
                     }
 
-                    template<typename T> //overloading template with 3 inputs
+                template<typename T> //overloading template with 3 inputs
                     void mergesort(T array[], T i, T k){
+                        uint64_t  breakpoint = i + ((k+i)/2);
+                        std::cout << "midpoint value array[" << breakpoint << "] = " << array[breakpoint]; << std::endl;
                         std::cout << "mergesort (overloaded 3 input) test" <<std::endl;
                         for(int index = i; index < (k-i+1); index++){
-                            std::cout << "list[" << index << "] equals " << array[index] << std::endl;
+                            std::cout << "array[" << index << "] equals " << array[index] << std::endl;
                         }
 
                     }
