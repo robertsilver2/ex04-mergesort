824cb1fe6491aface9a3ad433439dd9e6a34731d
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-04 23:09:04 -0800

    -Got the recursive calls going, -merge function initialized and does example return -sorting not yet implemented
    11/04
    -still having some problems, basically redid everything to not use
    templates just to test the sorting, going to try and put back into templates
    -strings still not implemented
    --update 10:19 pm, sorted strings with templates implemented.
    Problem was I wasn't allocating a new dynamic array in merge.
    --update 11:08 - sorting ints and strings with a 3 parameter input

--- src/main.cc
+++ src/main.cc
@@ -20,30 +20,55 @@ using edu::vcccd::vc::csv15::mergesort;
 int main(int argc, char *argv[]) {
 
     //initialize array and related variables
-    //int64_t array[] = {1578, 493, 903, 496, 519, 365, 108, 33, 1639, 613, 1468, 1063, 1776, 464, 1950, 1463, 1999, 1947, 747, 1320};
+    int64_t array1[] = {1578, 493, 903, 496, 519, 365, 108, 33, 1639, 613, 1468, 1063, 1776, 464, 1950, 1463, 1999, 1947, 747, 1320};
     //array generated @ https://www.random.org/integer-sets/
-
-    std::string array[] = { "bee", "ape", "key", "day", "zoo", "man" };
-    uint64_t size = 6;
+    uint64_t size = 20;
     uint64_t start = 0;
     uint64_t  end = size-1;
 
     //DEBUG: print array
-    std::cout << "\n starting array: \n" <<std::endl;
+    std::cout << "\n starting array1: \n" <<std::endl;
     for(int index = start; index < (end-start+1); index++){
-        std::cout << "array[" << index << "] equals " << array[index] << std::endl;
+        std::cout << "array1[" << index << "] equals " << array1[index] << std::endl;
     }
     std::cout << std::endl;
 
     //Sort initial array
     //mergesort2(array, start, end);    //non template int
-    mergesort(array, start, end);       //3 param templated
+    mergesort(array1, start, end);       //3 param templated
     //mergesort(array, searchTarget);   //2 param templated?
 
     //DEBUG: print sorted array
     std::cout << "\n sorted array: \n" <<std::endl;
     for(int index = start; index < (end-start+1); index++){
-        std::cout << "array[" << index << "] equals " << array[index] << std::endl;
+        std::cout << "array1[" << index << "] equals " << array1[index] << std::endl;
+    }
+    std::cout << std::endl;
+
+
+    //Test with strings
+    std::string array2[] = { "bee", "ape", "key", "day", "zoo", "man" };
+
+    uint64_t sizeb = 6;
+    uint64_t startb = 0;
+    uint64_t endb = sizeb-1;
+
+    //DEBUG: print array
+    std::cout << "\n starting array2: \n" << std::endl;
+    for(int index = startb; index < (endb-startb+1); index++){
+        std::cout << "array2[" << index << "] equals " << array2[index] << std::endl;
+    }
+    std::cout << std::endl;
+
+    //Sort initial array
+    //mergesort2(array, start, end);    //non template int
+    mergesort(array2, startb, endb);       //3 param templated
+    //mergesort(array, searchTarget);   //2 param templated?
+
+    //DEBUG: print sorted array
+    std::cout << "\n sorted array2: \n" <<std::endl;
+    for(int index = startb; index < (endb-startb+1); index++){
+        std::cout << "array2[" << index << "] equals " << array2[index] << std::endl;
     }
     std::cout << std::endl;
 
