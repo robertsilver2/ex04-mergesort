59449eb4c890f89c31ab9677b0503777bb84637a
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-04 22:01:37 -0800

    -Got the recursive calls going, -merge function initialized and does example return -sorting not yet implemented
    11/04
    -still having some problems, basically redid everything to not use templates just to test the sorting, going to try and put back into templates
    -strings still not implemented

--- include/mergesort.h
+++ include/mergesort.h
@@ -21,8 +21,129 @@ namespace edu {
         namespace vc{
             namespace csv15{
 
-                template<typename T>
-                void merge(T array[], uint64_t i, uint64_t split, uint64_t k){
+                void merge2(int64_t array[], uint64_t i, uint64_t split, uint64_t k){
+                    //DEBUG: notify entered Merge
+                    std::cout << "\n Merge Function Reached \n" << std::endl;
+                    std::cout << "in merge: \n i=" << i << ", k =" << k << std::endl;
+                    //DEBUG: print sub-array
+                    for(int index = i; index < (k-i+1); index++){
+                        std::cout << "array[" << index << "] equals " << array[index] << std::endl;
+                    }
+                    size_t length;
+                    if((k^i)!=NULL) { // if K ^ i not null how can it not evaluate to a constant?
+                        length = (k - i + 1);
+                    }else{
+                        length = 0;
+
+                    }
+                    //int64_t sorted[length];
+
+                    //DEBUG
+                    std::cout << "length = " << length << std::endl;
+
+                    int64_t *sorted = new int64_t [length];
+                    //^dynamic array?
+                    //source1: https://stackoverflow.com/questions/33423502/expression-did-not-evaluate-to-a-constant-c
+                    //source2: https://linuxconfig.org/example-of-c-class-template-array-to-instantiate-an-array-of-any-element-type
+                    //
+                    std::cout << "split = " << split << std::endl;
+                    uint64_t sortPos=0;
+                    uint64_t left = i; //beginning of LHS
+                    uint64_t right = split+1; //beginning of RHS
+
+                    while((left <= split) && (right <= k)){
+                        //put smallest elements into new array
+                        //in order smallest to highest
+                        //Debug: print index new array
+                        std::cout << "sortPos = " << sortPos << std::endl;
+                        if(array[left] <= array[right]){
+                            sorted[sortPos] = array[left];
+                            //Debug
+                            std::cout << "left = " << left << " + 1 = ";
+                            left++;
+                            std::cout << left << std::endl;
+                        }else{
+                            sorted[sortPos] = array[right];
+                            std::cout << "right = " << right << " + 1 = ";
+                            right++;
+                            std::cout << right << std::endl;
+                        }
+                        std::cout << "sorted[" << sortPos << "] = " << sorted[sortPos] << std::endl;
+                        sortPos++;
+                    }
+                    //Debug: print index new array
+                    std::cout << "sortPos = " << sortPos << std::endl;
+                    //fill in all the empty slots with the remaining
+                    //members of LHS and RHS
+                    while(left <= split){
+                        sorted[sortPos] = array[left];
+                        //Debug:
+                        std::cout << left << std::endl;
+                        std::cout << "sorted[" << sortPos << "] = " << sorted[sortPos] << std::endl;
+                        sortPos++;
+                        //Debug
+                        std::cout << "left = " << left << " + 1 = ";
+                        left++;
+                    }
+                    while(right<=k){
+                        sorted[sortPos]=array[right];
+                       //Debug:
+                        std::cout << "sorted[" << sortPos << "] = " << sorted[sortPos] << std::endl;
+                        sortPos++;
+                        //Debug:
+                        std::cout << "right = " << right << " + 1 = ";
+                        right++;
+                        std::cout << right << std::endl;
+                    }
+                    //apply changes to original array
+                    for(sortPos = 0; sortPos < length; sortPos++){
+                        array[i+sortPos]=sorted[sortPos];
+                    }
+                    //delete[] sorted;
+                    //avoid memory leak?
+                    //https://stackoverflow.com/questions/33423502/expression-did-not-evaluate-to-a-constant-c
+                    //
+                }
+                //second attempt: still experiencing error on temp array init
+                //C:\Users\Scott\CLionProjects\ex04-mergesort\include\mergesort.h(36): error C2131: expression did not evaluate to a constant
+                //C:\Users\Scott\CLionProjects\ex04-mergesort\include\mergesort.h(36): note: failure was caused by a read of a variable outside its lifetime
+                //C:\Users\Scott\CLionProjects\ex04-mergesort\include\mergesort.h(36): note: see usage of 'length'
+
+                void mergesort2(int64_t array[], uint64_t i, uint64_t k){
+                    //Debug: what were inputs
+                    std::cout << "mergesort entered" << std::endl;
+                    std::cout << "i = " << i << std::endl;
+                    std::cout << "k = " << k << std::endl;
+                    std::cout << std::endl;
+                    //partition/recursive call
+                    if(i<k){
+                        uint64_t  breakpoint = i + ((k-i)/2);
+                        mergesort2(array, i, breakpoint); //recursive call LHS
+                        mergesort2(array, breakpoint+1, k); //recursive call RHS
+
+                        //DEBUG:
+                        std::cout << "ready to merge from " << i << " to " << k << std::endl;
+                        std::cout << "midpoint value array[" << breakpoint << "] = " << array[breakpoint] << std::endl;
+                        //std::cout << "mergesort (overloaded 3 input) test" <<std::endl;
+
+                        //pass LHS & RHS (individually sorted) into merge
+                        merge2(array, i, breakpoint, k);
+
+                        //DEBUG: print sub-array
+                        for(int index = i; index < (k-i+1); index++){
+                            std::cout << "array[" << index << "] equals " << array[index] << std::endl;
+                        }
+                        std::cout << std::endl;
+                    }else{
+                        //DEBUG: single value in interval i, k: print
+                        std::cout << "array[" << i << "] = " << array[i] << std::endl;
+                        std::cout << std::endl;
+                    }
+                }
+
+                /*
+                //template<typename T>
+                //void merge(T array[], uint64_t i, uint64_t split, uint64_t k){
 
                     //DEBUG: notify entered Merge
                     std::cout << "\n Merge Function Reached \n" << std::endl;
@@ -73,6 +194,8 @@ namespace edu {
                     //https://stackoverflow.com/questions/33423502/expression-did-not-evaluate-to-a-constant-c
                     //
                 }
+                 */
+                //merge: 3 parameters, templated
 
 
 
@@ -82,10 +205,11 @@ namespace edu {
 
                     }
                     */
+                //attempt: mergesort class, two parameters inputs
 
 
 
-                template<typename T> //overloading template with 3 inputs
+                /*template<typename T> //overloading template with 3 inputs
                 void mergesort(T array[], uint64_t i, uint64_t k){
                      //Debug: what were inputs
                     std::cout << "mergesort entered" << std::endl;
@@ -117,7 +241,8 @@ namespace edu {
                         std::cout << std::endl;
                     }
 
-                }
+                }*/
+                //attempt 2: mergesort class overloaded template 3 parameters
 
 
             }
