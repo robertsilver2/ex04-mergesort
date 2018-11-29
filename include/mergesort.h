e3f5335ebf0e9ebe2ef92d8a0d08c8e3f69a44ad
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-28 22:25:57 -0800

    kajsdasj

--- include/mergesort.h
+++ include/mergesort.h
@@ -6,7 +6,7 @@
  *
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
- *asdasdasd
+ *
  * @robertsilver2
  */
 
